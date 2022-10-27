2b49a6b3d3bd49ccca42c98cd0b996a4a2395579
lcourdes <lea.courdes.murphy@gmail.com>
2022-10-27 15:14:48 +0200

    Création de la classe Player

---
+++ player.py
@@ -0,0 +1,11 @@
+class Player:
+    """
+    CLasse qui définit un joueur.
+    """
+
+    def __init__(self, name, first_name, date_of_birth, gender, ranking):
+        self.name = name
+        self.first_name = first_name
+        self.date_of_birth = date_of_birth
+        self.gender = gender
+        self.ranking = ranking
