d30723a5fd258a7b6c8d1fdea1766d3b0fd23057
lcourdes <lea.courdes.murphy@gmail.com>
2022-11-03 09:58:10 +0100

    Ajout de méthodes de serialisation et de création d'instance dans le modèle Match

--- models/match.py
+++ models/match.py
@@ -6,7 +6,7 @@ class Match:
     Classe qui définit un match.
     """
 
-    def __init__(self, list_of_two_players:list):
+    def __init__(self, list_of_two_players: list):
         self.list_of_two_players = list_of_two_players
         self.assign_color_to_player()
         self.color_distribution = {}
@@ -48,6 +48,65 @@ class Match:
             result_of_player_2 = [self.list_of_two_players[1], 0.5]
         elif result_of_player_1[1] == 1:
             result_of_player_2 = [self.list_of_two_players[1], 0]
-        result = (result_of_player_1, result_of_player_2)
+        self.result = (result_of_player_1, result_of_player_2)
 
-        return result
+        return self.result
+
+    def serialize(self):
+        """
+        Cette méthode permet de sérialiser l'instance.
+
+        Returns:
+             serialized_match = dictionnaire de l'instance.
+        """
+
+        serialized_match = {'list_of_two_players': [player.id for player in self.list_of_two_players]}
+
+        if self.color_distribution == {}:
+            serialized_match['color_distribution'] = {}
+        else:
+            serialized_match['color_distribution'] = {player.id: color for player, color in
+                                                      self.color_distribution.items()}
+        if self.result == ():
+            serialized_match['result'] = ()
+        else:
+            serialized_match['result'] = ([self.result[0][0].id, self.result[0][1]],
+                                          [self.result[1][0].id, self.result[1][1]])
+
+        return serialized_match
+
+    @classmethod
+    def create_instance(cls, match: dict, list_of_players: list):
+        """
+        Cette méthode permet de créer une instance de Match à partir d'un dictionnaire d'un match.
+
+        Arg:
+            match = un dictionnaire contenant les informations d'un match.
+
+        Returns:
+            une instance de Match.
+        """
+        two_ids_players = match['list_of_two_players']
+        list_of_two_players = []
+        for id in two_ids_players:
+            for player in list_of_players:
+                if player.id == id:
+                    list_of_two_players.append(player)
+        created_match = cls(list_of_two_players)
+        color_distribution = {}
+        if match['color_distribution'] != {}:
+            for player_id, color in match['color_distribution'].items():
+                for player in list_of_players:
+                    if player_id == player.id:
+                        color_distribution[player] = color
+        created_match.color_distribution = color_distribution
+
+        result = []
+        if match['result'] != ():
+            for result_of_player in match['result']:
+                for player in list_of_players:
+                    if result_of_player[0] == player.id:
+                        result.append([player, result_of_player[1]])
+        created_match.result = tuple(result)
+
+        return created_match
