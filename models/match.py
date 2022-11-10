fc3327c31f3c5cf043b1ec1a1b921e8c65aa40ba
lcourdes <lea.courdes.murphy@gmail.com>
2022-11-10 10:41:20 +0100

    La méthode get_result du modèle Match ne produit plus un résultat aléatoire mais prend des arguments

--- models/match.py
+++ models/match.py
@@ -8,9 +8,9 @@ class Match:
 
     def __init__(self, list_of_two_players: list):
         self.list_of_two_players = list_of_two_players
-        self.assign_color_to_player()
         self.color_distribution = {}
-        self.result = ()
+        self.assign_color_to_player()
+        self.result = ([], [])
 
     def assign_color_to_player(self):
         """
@@ -22,7 +22,7 @@ class Match:
         """
 
         players = [self.list_of_two_players[0], self.list_of_two_players[1]]
-        colors = ["white", "black"]
+        colors = ["blancs", "noirs"]
         random_player = random.choice(players)
         random_color = random.choice(colors)
         players.remove(random_player)
@@ -31,25 +31,26 @@ class Match:
 
         return self.color_distribution
 
-    def get_result(self):
+    def get_result(self, player_id, score):
         """
-        Pour l'instant cette méthode détermine aléatoirement le résultat du match.
+        Cette méthode permet de stocker le score du match.
+        A partir du score de l'un des deux joueurs du match, le score de l'autre joueur est calculé automatiquement.
+
+        Args:
+            player_id = l'id d'un joueur.
+            score = le score du match du joueur dont l'id est également un argument.
 
         Returns:
              Un tuple contenant deux listes ([joueur_1, résultat], [joueur_2, résultat])
         """
+        if self.list_of_two_players[0].id == player_id:
+            result_of_player_1 = [self.list_of_two_players[0], score]
+            result_of_player_2 = [self.list_of_two_players[1], 1 - score]
+        else:
+            result_of_player_1 = [self.list_of_two_players[0], 1 - score]
+            result_of_player_2 = [self.list_of_two_players[1], score]
 
-        result_of_player_1 = [self.list_of_two_players[0], random.choice([0, 0.5, 1])]
-        result_of_player_2 = []
-
-        if result_of_player_1[1] == 0:
-            result_of_player_2 = [self.list_of_two_players[1], 1]
-        elif result_of_player_1[1] == 0.5:
-            result_of_player_2 = [self.list_of_two_players[1], 0.5]
-        elif result_of_player_1[1] == 1:
-            result_of_player_2 = [self.list_of_two_players[1], 0]
         self.result = (result_of_player_1, result_of_player_2)
-
         return self.result
 
     def serialize(self):
@@ -67,8 +68,8 @@ class Match:
         else:
             serialized_match['color_distribution'] = {player.id: color for player, color in
                                                       self.color_distribution.items()}
-        if self.result == ():
-            serialized_match['result'] = ()
+        if self.result == ([], []):
+            serialized_match['result'] = ([], [])
         else:
             serialized_match['result'] = ([self.result[0][0].id, self.result[0][1]],
                                           [self.result[1][0].id, self.result[1][1]])
@@ -82,31 +83,34 @@ class Match:
 
         Arg:
             match = un dictionnaire contenant les informations d'un match.
+            list_of_players = la liste des joueurs inscrits au tournoi pour lequel le match a été joué.
 
         Returns:
             une instance de Match.
         """
         two_ids_players = match['list_of_two_players']
         list_of_two_players = []
-        for id in two_ids_players:
+        for player_id in two_ids_players:
             for player in list_of_players:
-                if player.id == id:
+                if player.id == player_id:
                     list_of_two_players.append(player)
         created_match = cls(list_of_two_players)
         color_distribution = {}
         if match['color_distribution'] != {}:
             for player_id, color in match['color_distribution'].items():
                 for player in list_of_players:
-                    if player_id == player.id:
+                    if int(player_id) == player.id:
                         color_distribution[player] = color
         created_match.color_distribution = color_distribution
 
         result = []
-        if match['result'] != ():
+        if match['result'] != [[], []]:
             for result_of_player in match['result']:
                 for player in list_of_players:
                     if result_of_player[0] == player.id:
                         result.append([player, result_of_player[1]])
+        else:
+            result = [[], []]
         created_match.result = tuple(result)
 
         return created_match
